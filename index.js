f361df653ce9e3b14f29e5446ac7910e128e1570
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-11-30 00:33:01 +0530

    Create, update, delete and read done using MVC minor fixes needs too be done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router=express.Router();
 const fs = require('fs');
-// const {create,read,update,delete,postdata,readdata,updatedata,deletedata}= require('./controllers/crud')
+const {create,postData,read,readData,update, updateData,deleteFun, deleteFile}= require('./controllers/crud')
 router.get("/",(req,res)=>{
     res.render('index',{title:'Home Page'})
 })
-// router.get("/create",Create);
-// router.get("/read",read);
-// router.get("/update",update);
-// router.get("/delete",Delete);
-// router.post("/postdata",postdata)
-// router.post("/readdata",readdata);
-// router.post("/updatedata",updatedata)
-// router.post("/deletedata",deletedata)
+router.get("/create",create);
+router.get("/read",read);
+router.get("/update",update);
+router.get("/delete",deleteFun);
+router.post("/postdata",postData)
+router.post("/readdata",readData);
+router.post("/updatedata",updateData)
+router.post("/deletefile",deleteFile)
 module.exports=router;
